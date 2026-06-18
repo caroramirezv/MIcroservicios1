c7458fecd00a583402b046908ed74290df4eb51a
caroramirezv <caro.ramirezv@duocuc.cl>
2026-06-18 14:38:55 -0400

    Add files via upload

--- ms-carrito/carrito/src/main/java/com/prueba/carrito/CarritoApplication.java
+++ ms-carrito/carrito/src/main/java/com/prueba/carrito/CarritoApplication.java
@@ -1,7 +1,9 @@
 package com.prueba.carrito;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class CarritoApplication {

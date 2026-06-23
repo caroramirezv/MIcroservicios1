1c74cac8131842af92c23e1d0d366807448d1075
caroramirezv <caro.ramirezv@duocuc.cl>
2026-06-22 20:26:46 -0400

    Add files via upload
    Se agrego Data Faker a ropa

--- ms-ropa/ropa/src/main/java/com/prueba/ropa/Controller/RopaController.java
+++ ms-ropa/ropa/src/main/java/com/prueba/ropa/Controller/RopaController.java
@@ -6,18 +6,27 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prueba.ropa.Model.Ropa;
+import com.prueba.ropa.Service.RopaService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import com.prueba.ropa.Model.Ropa;
-import com.prueba.ropa.Service.RopaService;
-
 @RestController
 @RequestMapping("/api/v1/ropa")
 @Tag(name = "Ropas", description = "Gestionar la ropa ingresada")
+
 
 public class RopaController {
     @Autowired
@@ -25,18 +34,29 @@ public class RopaController {
 
     @PostMapping
     @Operation(summary ="Crea una ropa", description = "Crea una ropa desde 0")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Ropa creada correctamente"),
+        @ApiResponse(responseCode = "404", description = "Ropa no se pudo crear, intentelo nuevamente")
+    })   
     public ResponseEntity<Ropa> crear (@RequestBody Ropa ropa){
         return new ResponseEntity<>(service.save(ropa), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Se encontraron ropas"),
+        @ApiResponse(responseCode = "404", description = "Ropa no encontrada")
+    })      
     @Operation(summary ="Obtener toda la ropa", description = "Obtiene una lista de la ropa disponible")
     public ResponseEntity <List<Ropa>> listar(){
         return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping("/{id}")
-    @Operation(summary ="Obtener ropa por id", description = "Obtiene una lista de la ropa por su id")
+    @Operation(summary ="Obtener ropa por id", description = "Obtiene una lista de la ropa por su id")@ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Ropa encontrada con exito"),
+        @ApiResponse(responseCode = "404", description = "Ropa no encontrada")
+    })   
     public ResponseEntity<Map<String, Object>> obtenerCompleto(@PathVariable Integer id){
         Map<String, Object> respuesta = service.buscarRopaCompleto(id);
         return respuesta.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(respuesta); 
--- ms-ropa/ropa/src/main/java/com/prueba/ropa/DataLoader.java
+++ ms-ropa/ropa/src/main/java/com/prueba/ropa/DataLoader.java
@@ -1,11 +1,13 @@
 package com.prueba.ropa;
 
-import com.prueba.ropa.Model.Ropa;
-import com.prueba.ropa.Repository.RopaRepository;
-import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import com.prueba.ropa.Model.Ropa;
+import com.prueba.ropa.Repository.RopaRepository;
+
+import net.datafaker.Faker;
 
 @Profile("dev")
 @Component
@@ -19,19 +21,20 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         Faker faker = new Faker();
 
-        // Generamos 50 prendas
+        // Se generan 50 prendas
         for (int i = 0; i < 50; i++) {
             Ropa prenda = new Ropa();
-            
-            prenda.setNombre(faker.commerce().productName()); 
-            
-            prenda.setDetalle("Prenda de excelente calidad. Material: " + 
-                               faker.commerce().material() + ", Color: ");
-            
+
+            prenda.setNombre(faker.commerce().productName());
+
+            prenda.setDetalle("Prenda de excelente calidad. Material: " +
+                               faker.commerce().material() + ", Color: " + faker.color().name());
+
             // Precios aleatorios entre 4.990 y 49.990
-            prenda.setPrecio(faker.number().numberBetween(4990, 49990)); 
+            prenda.setPrecio(faker.number().numberBetween(4990, 49990));
 
             ropaRepository.save(prenda);
         }

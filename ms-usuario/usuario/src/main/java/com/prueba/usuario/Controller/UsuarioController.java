8784b8ba668c3e237e349417017052795b73b55d
caroramirezv <caro.ramirezv@duocuc.cl>
2026-06-22 20:12:54 -0400

    Add files via upload

--- ms-usuario/usuario/src/main/java/com/prueba/usuario/Controller/UsuarioController.java
+++ ms-usuario/usuario/src/main/java/com/prueba/usuario/Controller/UsuarioController.java
@@ -3,11 +3,6 @@ package com.prueba.usuario.Controller;
 import java.util.List;
 import java.util.Map;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.prueba.usuario.Model.Usuario;
 import com.prueba.usuario.Service.UsuarioService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1/usuario")

037161ebe525009d165ebace4b78164af357fd62
Carolina Ramirez <caro.ramirezv@duocuc.cl>
2026-06-24 18:01:48 -0400

    Se agrego el API Gateway centralizado

--- ms-busqueda/busqueda/src/main/java/com/prueba/busqueda/BusquedaApplication.java
+++ ms-busqueda/busqueda/src/main/java/com/prueba/busqueda/BusquedaApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
-@EnableFeignClients
-@SpringBootApplication
+@EnableFeignClients(basePackages = "com.prueba.busqueda")
+@SpringBootApplication(scanBasePackages = "com.prueba.busqueda")
 public class BusquedaApplication {
 
 	public static void main(String[] args) {

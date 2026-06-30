d9929afa54d4ea05cb03948287169726ffb59bb9
mhyjal <mh.rozas@duocuc.cl>
2026-06-30 09:36:38 -0400

    se  actualizo hateoas

--- ms-ropa/ropa/src/main/java/com/prueba/ropa/Assemblers/RopaModelAssembler.java
+++ ms-ropa/ropa/src/main/java/com/prueba/ropa/Assemblers/RopaModelAssembler.java
@@ -1,5 +1,19 @@
 package com.prueba.ropa.Assemblers;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-public class RopaModelAssembler {
+import com.prueba.ropa.Controller.RopaControllerV2;
+import com.prueba.ropa.Model.Ropa;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+@Component
+public class RopaModelAssembler implements RepresentationModelAssembler <Ropa, EntityModel<Ropa>> {
+
+	@Override
+	public EntityModel<Ropa> toModel(Ropa ropa) {
+		return EntityModel.of(ropa,
+				linkTo(methodOn(RopaControllerV2.class).obtenerCompleto(ropa.getId())).withSelfRel(),
+				linkTo(methodOn(RopaControllerV2.class).listar()).withRel("ropa"));
+	}
 }
